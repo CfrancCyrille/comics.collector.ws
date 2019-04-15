0e9752386046afc98fdde59800b0cf1aee1dd956
Paul <paul.galceran@wanadoo.fr>
2019-04-15 18:47:26 +0200

    rectification requête

--- src/test/java/web/jax/rs/data/jpa/service/AlbumRepositoryTest.java
+++ src/test/java/web/jax/rs/data/jpa/service/AlbumRepositoryTest.java
@@ -52,15 +52,27 @@ public class AlbumRepositoryTest {
 
     @Test
     public void getCouple_Asterix_UderzoGoscinny(){
-        Query q = em.getEntityManager().createQuery("SELECT NEW web.jax.rs.data.jpa.dto.AlbumShortDTO((SELECT ap.personne.nomUsuel FROM Albumpersonne ap JOIN ap.album a2 WHERE a2 = a AND ap.metier.libelleMetier = 'Scenario'), (SELECT ap.personne.nomUsuel FROM Albumpersonne ap JOIN ap.album a2 WHERE a2 = a AND ap.metier.libelleMetier = 'Dessin'), a.titreAlbum, s.titreSerie) FROM Album a LEFT JOIN a.serie s");
+        Query q = em.getEntityManager().createQuery("SELECT NEW web.jax.rs.data.jpa.dto.AlbumShortDTO(" +
+                "(SELECT ap.personne.nomUsuel FROM web.jax.rs.data.jpa.domain.Albumpersonne ap JOIN ap.album a2 " +
+                "WHERE a2 = a AND ap.metier.libelleMetier = 'Scenario' AND ap.personne.nomUsuel = :senariste), " +
+                "(SELECT ap.personne.nomUsuel FROM web.jax.rs.data.jpa.domain.Albumpersonne ap JOIN ap.album a2 " +
+                "WHERE a2 = a AND ap.metier.libelleMetier = 'Dessin' AND ap.personne.nomUsuel = :dessinateur)," +
+                " a.titreAlbum, a.serie.titreSerie) " +
+                "FROM Album a " +
+                "LEFT JOIN a.serie " +
+                "WHERE a.serie.titreSerie = :titreSerie");
+        q.setParameter("senariste", "Goscinny, René");
+        q.setParameter("dessinateur", "Uderzo, Albert");
+        q.setParameter("titreSerie", "Astérix");
         List<AlbumShortDTO> r = q.getResultList();
-        int expected = 207;
+        int expected = 7;
         int actual = r.size();
         assertEquals(expected, actual);
     }
 
     @Test
     public void nbAlbums_All_39(){
+
         Query q = em.getEntityManager().createQuery("SELECT NEW web.jax.rs.data.jpa.dto.AlbumCountAuthorEditorDTO(count(a), a.editeur, ap.personne) FROM Album a LEFT JOIN a.albumpersonnes ap GROUP BY a.editeur.nomEditeur, ap.personne.nomUsuel ORDER BY count(a)");
         List<AlbumCountAuthorEditorDTO> r = q.getResultList();
         int expected = 39;
